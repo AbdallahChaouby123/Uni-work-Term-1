1039a3a54d7b191a8469d9ad6b78de844470eb00
AbdallahChaouby123 <48841601+AbdallahChaouby123@users.noreply.github.com>
2023-11-16 10:57:44 +0000

    Add files via upload

---
+++ if else example.py
@@ -0,0 +1,6 @@
+nocap = ("yo mama ugly")
+withcap = input ("press w to continue")
+if (withcap) == "w":
+    print ("good job, you can read")
+else:
+    print (nocap)
---
+++ python.py
@@ -0,0 +1,9 @@
+from random import randint
+print('fuck you')
+def diceroll():
+    max = input('max amount of sides')
+    print('thats a D', max)
+    roll = randint(1, int(max))
+    print('rolled a', roll)
+diceroll()
+    
